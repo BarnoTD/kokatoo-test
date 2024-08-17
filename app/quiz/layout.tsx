3cea5018474eedaabfa5518ec93bd30f320cfba5
barnoTD <firashinow@gmail.com>
2024-08-17 04:00:27 +0100

    nested children in parent in layout.tsx

--- app/quiz/layout.tsx
+++ app/quiz/layout.tsx
@@ -3,7 +3,9 @@ import SideNav from "@/components/ui/sidenav";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
       <div className="">
-          <SideNav children = {children} />
+          <SideNav>
+            {children}
+          </SideNav> 
       </div>
     );
   }
