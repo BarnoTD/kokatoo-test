68cb16001bef47fc78e6cd45565c549ca45407df
barnoTD <firashinow@gmail.com>
2024-08-17 04:39:39 +0100

    changed page title

--- app/quiz/layout.tsx
+++ app/quiz/layout.tsx
@@ -1,4 +1,9 @@
 import SideNav from "@/components/ui/sidenav";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Kokatoo',
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
